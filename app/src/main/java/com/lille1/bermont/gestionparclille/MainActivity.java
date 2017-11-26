b6622acbbc808663eb8726c3250bc0bc14d710c8
Benjamin Bermont <benji-du-51@hotmail.fr>
2017-11-26 22:43:29 +0100

    Ajout du spinner

--- app/src/main/java/com/lille1/bermont/gestionparclille/MainActivity.java
+++ app/src/main/java/com/lille1/bermont/gestionparclille/MainActivity.java
@@ -2,6 +2,8 @@ package com.lille1.bermont.gestionparclille;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.type_probleme);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.type_probleme, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
